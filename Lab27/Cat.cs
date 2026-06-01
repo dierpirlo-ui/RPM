e032d549d52b8a25513143056449da0fd91bb61d
dierpirlo-ui <dierpirlo@gmail.com>
2026-06-01 17:17:17 +0300

    Add files via upload

---
+++ Lab27/Cat.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LabWork27.Models
+{
+    public record struct Cat(int Id, string Name, string Breed, string Color, int Age);
+}
