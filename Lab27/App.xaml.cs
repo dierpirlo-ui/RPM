9180ed3102535155c1167760081b2d6435a5de21
dierpirlo-ui <dierpirlo@gmail.com>
2026-06-01 17:18:47 +0300

    Add files via upload

---
+++ Lab27/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace LabWork27
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static Frame CurrentFrame { get; set; }
+    }
+}
---
+++ Lab27/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LabWork27
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ContentFrame.Navigate(new Pages.MainPage());
+            App.CurrentFrame = ContentFrame;
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            App.CurrentFrame.GoBack();
+        }
+
+        private void ContentFrame_ContentRendered(object sender, EventArgs e)
+        {
+            BackButton.Visibility = ContentFrame.CanGoBack ? Visibility.Visible : Visibility.Collapsed;
+        }
+    }
+}
